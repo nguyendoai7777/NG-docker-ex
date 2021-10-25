3c294c73d6812fe87c01547263bbbea167df5828
unknown <doainn@rikkeisoft.com>
2021-10-25 17:17:08 +0700

    splash loading re-manke-logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,16 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   deferredPrompt!: any;
+  card = true;
   @HostListener('window:beforeinstallprompt', [ '$event' ])
   onbeforeinstallprompt(e: Event): void {
     e.preventDefault();
     this.deferredPrompt = e;
   }
   title = 'ng-docker';
-
-
+  ngOnInit(): void {
+    setTimeout(() =>{
+      this.card = false;
+    }, 2000)
+  }
 }
